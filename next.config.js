87e484927d8d78560f747e3e9907b344a1a6f26a
Matthew Jorgensen <me@prplecake.com>
2023-05-14 15:08:44 -0500

    'Fix' assets path...

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const commitHash = require('child_process')
   .execSync('git rev-parse --short HEAD')
   .toString().trim();
+const repoName = 'fetchLocation';
 const repoUrl = 'https://github.com/prplecake/fetchLocation';
 
 /**
@@ -10,6 +11,7 @@ const nextConfig = {
   output: 'export',
   env: {
     COMMIT_HASH: commitHash,
+    REPO_NAME: repoName,
     REPO_URL: repoUrl,
   }
 }

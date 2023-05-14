2dc4a4359f323c872c653d23d70617089aace381
Matthew Jorgensen <me@prplecake.com>
2023-05-14 13:50:11 -0500

    Add commit hash

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import {Metadata} from "next";
+import Footer from "../components/layout/footer";
 
 export const metadata: Metadata = {
   title: 'fetchLocation',
@@ -12,8 +13,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang={'en'}>
+    <head>
+      <link rel={'stylesheet'} href={'/styles/main.css'} />
+    </head>
     <body>
     {children}
+    <Footer/>
     </body>
     </html>
   )
---
+++ components/layout/footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <>
+      {process.env.COMMIT_HASH ? (
+        <div className={'version-info'}>
+          <a href={process.env.REPO_URL + '/commit/' + process.env.COMMIT_HASH}
+             target={'_blank'}
+             rel={'noreferrer'}
+          >{process.env.COMMIT_HASH}</a>
+          </div>
+      ) : null}
+    </>
+  )
+}

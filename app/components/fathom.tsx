79390c40460b2a430ad7ac06e113caeda0d3630f
prplecake <me@prplecake.com>
2023-06-05 12:18:16 -0500

    Add Fathom Analytics

---
+++ app/components/fathom.tsx
@@ -0,0 +1,19 @@
+'use client';
+import { load, trackPageview } from "fathom-client";
+import { useEffect } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+
+export function Fathom() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  useEffect(() => {
+    load('', {
+      includedDomains: ['prplecake.github.io'],
+      spa: 'auto',
+    });
+
+    trackPageview();
+  }, [pathname, searchParams]);
+
+  return null;
+}

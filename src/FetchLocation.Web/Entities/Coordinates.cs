9343317ac1bf8ddbc69067b3f85c43455db80fab
Matthew Jorgensen <me@prplecake.com>
2024-06-17 13:56:29 -0500

    Init Blazor app

---
+++ src/FetchLocation.Web/Entities/Coordinates.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FetchLocation.Web.Entities;
+
+public record Coordinates(
+    [property: JsonPropertyName("latitude")]
+    double Latitude,
+    [property: JsonPropertyName("longitude")]
+    double Longitude)
+{
+    public override string ToString() => $"{Latitude}, {Longitude}";
+    public string ToUriString() => $"{Latitude},{Longitude}";
+}
